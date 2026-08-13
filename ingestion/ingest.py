c331878a0a7d899c019be5c436d5a83d7d16249f
thepassant <bassant.ayman69@gmail.com>
2026-08-13 23:44:16 +0300

    Remove unused os import and revert to default data path in load_index function

--- ingestion/ingest.py
+++ ingestion/ingest.py
@@ -1,13 +1,11 @@
-import os
+
 import pandas as pd
 
 import minsearch
 
 
-DATA_PATH = os.getenv("DATA_PATH", "../data/cleaned_data.csv")
 
-
-def load_index(data_path=DATA_PATH):
+def load_index(data_path="../data/cleaned_data.csv"):
     df = pd.read_csv(data_path)
 
     documents = df.to_dict(orient="records")
